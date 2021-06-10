c444b5e9ab647cf0d5e76cbe8116fbad91677721
Anup Kishore <anup@secondspectrum.com>
2021-06-10 16:40:47 -0400

    Convert Submissions to Entries for writing to output

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,77 @@
 import * as data from "./data";
+import { Submission } from "./types";
+
+type Entry = {
+  twitch_username: string;
+  entries: Submission[];
+  completed: Submission[];
+};
+type Lookup = { [twitch_username: string]: Entry };
+
+type Accumulator = { users: string[]; entries: Lookup };
+
+function updateEntry(entry: Entry, submission: Submission): Entry {
+  const { twitch_username, status_ } = submission;
+  console.assert(
+    entry.twitch_username === twitch_username,
+    "Tried to update an entry with a submission for a different twitch user! (twitch_username: ${twitch_username}, entry_username: ${entry.twitch_username}"
+  );
+
+  switch (status_) {
+    case "completed":
+      return {
+        ...entry,
+        completed: [...entry.completed, submission],
+      };
+    case "pending":
+      return {
+        ...entry,
+        entries: [submission],
+      };
+    case "expired":
+      return entry;
+  }
+}
+
+function newEntry(submission: Submission): Entry {
+  const { twitch_username } = submission;
+  const emptyEntry = { twitch_username, entries: [], completed: [] };
+
+  return updateEntry(emptyEntry, submission);
+}
+
+export function generateEntries(
+  acc: Accumulator,
+  submission: Submission
+): Accumulator {
+  const { users, entries } = acc;
+  const { twitch_username } = submission;
+
+  const entry = entries[twitch_username];
+  if (entry === undefined) {
+    return {
+      users: [...users, twitch_username],
+      entries: { ...entries, [twitch_username]: newEntry(submission) },
+    };
+  }
+
+  return {
+    users,
+    entries: { ...entries, [twitch_username]: updateEntry(entry, submission) },
+  };
+}
 
 async function main(): Promise<void> {
-  const foo = await data.fetchSubmissions();
+  const submissions = await data.fetchSubmissions();
+  const { users, entries } = submissions.reduce(generateEntries, {
+    users: [],
+    entries: {},
+  });
 
-  console.log(JSON.stringify(foo, null, 2));
+  const output = users
+    .flatMap((user) => (entries[user] ? [entries[user]] : []))
+    .slice(0, 5);
+  console.log(JSON.stringify(output, null, 2));
 }
 
 main();
