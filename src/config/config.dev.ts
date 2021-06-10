1b55fc93c7eacd06754475350f85166d4597ccec
Anup Kishore <anup@secondspectrum.com>
2021-06-10 16:55:42 -0400

    Move config file to a ts file for easier commenting and development

---
+++ src/config/config.dev.ts
@@ -0,0 +1,9 @@
+export default {
+  spreadsheet: {
+    id: "1AsRrTkE6LBxgE7byV0sFA98cOThVkjQW5Qq5EXv1Efk",
+    // `data` sheet
+    // data_sheet_id: 0,
+    // `test` sheet
+    data_sheet_id: 1938820088,
+  },
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import config from "./config.json";
+import config from "./config";
 import * as secrets from "./secrets";
 
 export { config, secrets };
